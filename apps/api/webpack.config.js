72937416e118298680d6ef214572574d4188b3e2
Yuri Mutti <muttiyuri@gmail.com>
2024-11-11 17:13:14 -0300

    chore: build scripts

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../../dist/apps/api/api'),
+    path: join(__dirname, '../../dist/apps/api/'),
   },
   plugins: [
     new NxAppWebpackPlugin({
