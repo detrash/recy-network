365ad7293de89553cbf376321dddcce5393d7846
Yuri Mutti <muttiyuri@gmail.com>
2024-11-14 23:41:22 -0300

    fix: paths

--- apps/web-mobile/vite.config.ts
+++ apps/web-mobile/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import path from 'path';
 
 export default defineConfig({
   root: __dirname,
@@ -23,9 +22,7 @@ export default defineConfig({
 
 
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'), // Ensure '@' points to the 'src' directory
-    },
+    alias: {},
   },
 
   // Uncomment this if you are using workers.
