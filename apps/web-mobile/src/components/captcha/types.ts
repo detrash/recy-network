76724a9be1a130b2201f5599fe58ddaf238c40f5
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-13 19:48:45 -0300

    refactor: improvment on types

--- apps/web-mobile/src/components/captcha/types.ts
+++ apps/web-mobile/src/components/captcha/types.ts
@@ -1,12 +1,15 @@
 export type turnstileWindow = Window & {
   turnstile?: {
     remove: (string?: string) => void;
-    render: (string: string, { sitekey, callback }: turnstileRenderBody) => void;
+    render: (string: string, options: turnstileRenderBody) => void;
     getResponse: () => void;
   };
 };
 type turnstileRenderBody = {
   sitekey: string;
-  callback: (token: any) => void;
+  callback: (token: string) => void;
   appearance: string;
+  language?: string;
+  theme?: 'light' | 'dark' | 'auto';
+  size?: 'normal' | 'compact';
 };
