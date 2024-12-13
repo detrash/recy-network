ff8b6c42c3d6fba4e20132f2e18ec9053e4b426e
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-13 01:44:56 -0300

    feat: add verification with server side

--- apps/web-mobile/src/components/captcha/index.tsx
+++ apps/web-mobile/src/components/captcha/index.tsx
@@ -1,26 +1,47 @@
 import { useEffect } from 'react';
 import { turnstileWindow } from './types';
+import { api } from '@/libs/axios';
+import { toast } from '../ui/use-toast';
 
-const SCRIPT_CAPTCHA = (
-  <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit" async defer></script>
-);
 const SITE_KEY = '0x4AAAAAAA1xm1WGL4XXa0S0';
 const { turnstile }: turnstileWindow = window;
 
-const captchaTrigger = async () => {
+const captchaTrigger = () => {
   try {
-    turnstile.remove();
     turnstile.render('.cf-turnstile', {
       sitekey: SITE_KEY,
       appearance: 'interaction-only',
-      callback: (token) => {
-        console.log('challenge: ', token);
+      callback: async (token) => {
+        await verifyCatpcha(token);
       },
     });
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    if (error instanceof Error) {
+      toast({
+        title: error.message,
+        variant: 'destructive',
+      });
+    }
   }
 };
+
+const verifyCatpcha = async (token: string) => {
+  const BASE_URL = import.meta.env.VITE_AWS_LAMBDA;
+  const ENVIRONMENT = import.meta.env.PROD ? 'production' : 'staging';
+  const ENDPOINT = `${BASE_URL}/${ENVIRONMENT}/v1/captcha`;
+
+  try {
+    await api.post(ENDPOINT, { token });
+  } catch (error) {
+    if (error instanceof Error) {
+      toast({
+        title: error.message,
+        variant: 'destructive',
+      });
+    }
+  }
+};
+
 const Captcha = () => {
   useEffect(() => {
     captchaTrigger();
