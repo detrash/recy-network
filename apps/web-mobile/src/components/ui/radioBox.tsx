81adb7558987149851fc9370da8b6435fe578ebd
Lucas-Bento-D <lucasbentoskl@outlook.com>
2024-12-30 12:47:20 -0300

    Feature: add radioBox component

---
+++ apps/web-mobile/src/components/ui/radioBox.tsx
@@ -0,0 +1,30 @@
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { Label } from '@/components/ui/label';
+
+const cssmap: any = {
+  'option-hodler': 'option-hodler',
+  'option-recycler': 'option-recycler',
+  'option-waste': 'option-waste',
+};
+interface Props {
+  beforeText?: string;
+  id: string;
+  name: string;
+  activeState: (string: string) => void;
+}
+export const RadioBox = ({ beforeText, id, name, activeState }: Props) => {
+  return (
+    <div
+      id={`${id}-box`}
+      className={`flex items-center space-x-2 rounded-lg border-2 px-4 py-6 [&>#${cssmap[id]}]:hidden`}
+    >
+      <RadioGroupItem value={id} id={id} onClick={() => activeState(`${id}-box`)} />
+      <div className="flex flex-col">
+        {beforeText && <span>{beforeText}</span>}
+        <Label className="mt-px text-base font-bold" htmlFor={id}>
+          {name}
+        </Label>
+      </div>
+    </div>
+  );
+};
